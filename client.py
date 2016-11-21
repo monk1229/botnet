018df09e60f3d9189d7d344375a520f08d24ccbe
monk1229 <mam1681@rit.edu>
2016-11-20 20:53:41 -0500

    initial code

---
+++ client.py
@@ -0,0 +1,76 @@
+import requests
+
+import time
+import uuid
+import json
+from os import remove
+from sys import exit
+import signal
+from random import randint
+from tempfile import mkdtemp, NamedTemporaryFile
+
+# Dictionary with urls as a key and filenames as values
+TEMP_FILES = {}
+TEMP_DIR_PATH = None
+
+CC_BASE_PATH = "http://localhost:8000"
+
+BOT_ID = uuid.getnode()
+
+def get_command():
+    r = requests.get("{}/command.json".format(CC_BASE_PATH), params={"id": BOT_ID})
+    print(r.text)
+    print(r.json())
+
+
+
+def fetch_page(url, **kwargs):
+    return requests.get(url, **kwargs)
+
+def download_item(url, **kwargs):
+    r = requests.get(url, **kwargs)
+
+    if r.status_code != 200:
+        return False
+
+    if url in TEMP_FILES:
+        remove(TEMP_FILES[url])
+
+    tmp_file = NamedTemporaryFile(delete=False)
+
+    if r.status_code == 200:
+        for chunk in r:
+            tmp_file.write(chunk)
+
+    tmp_file.close()
+    
+    TEMP_FILES[url] = tmp_file.name
+
+    return True
+
+def exit_gracefully():
+    print("Exiting")
+    print("Files to clean: {}".format(len(TEMP_FILES)))
+    for v in TEMP_FILES.values():
+        try:
+            remove(v)
+        except:
+            pass
+    exit()
+
+def main():
+    download_item("http://andresjruiz.com")
+
+    while(True):
+        print("Checking...")
+        get_command()
+        time.sleep(1)
+
+
+if __name__=="__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    finally:
+        exit_gracefully()
